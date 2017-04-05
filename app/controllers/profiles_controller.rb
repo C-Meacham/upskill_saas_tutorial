34f2dcc85e5c2ce251a2590352d4a987678e443e
Caleb Meacham <meachamca@gmail.com>
2017-04-05 02:57:37 +0000

    Added profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,21 @@ class ProfilesController < ApplicationController
   def new
     @profile = Profile.new
   end  
+  def create
+    # Ensure we have correct user
+    @user = User.find( params[:user_id] )
+    # Create profile linked to specifc user
+    @profile = @user.build_profile( profile_params )
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to root_path
+    else
+      flash[:notice] ="Oops! Something went wrong! Please try again."
+      render action: :new
+    end  
+  end  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end    
